bf84fbeab01a9937f661412f43d73d4c3f9a752f
Payal Likhar <payalslikhar@gmail.com>
2023-12-16 07:59:55 +0530

    commit done in code

--- jenkins_files/continuous_integration/app.js
+++ jenkins_files/continuous_integration/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res
     .status(200)
-    .json({ message: "This will demo node test for and CICD and Webhook." });
+    .json({ message: "This will demo node test for and CICD and Web  hook." });
 });
 
 module.exports = app;
